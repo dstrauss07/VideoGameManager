5dca3608f8cfdd00a0f95454b311df29bea8bfc0
dstrauss <fmdenver@gmail.com>
2018-12-13 22:53:09 -0500

    Initial Commit

---
+++ src/VideoGameManager/VideoGameManager/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VideoGameManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
